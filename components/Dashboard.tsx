22fa9fedca65c88c7fd391d7868e9d33dcfe0b3d
Jordan Allen <jallen@elementengineering.com.au>
2025-04-09 11:39:49 +0800

    first implementation

---
+++ components/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Leaseiq dashboard</h1>
+        </div>
+    );
+}
+
+export default Dashboard;
